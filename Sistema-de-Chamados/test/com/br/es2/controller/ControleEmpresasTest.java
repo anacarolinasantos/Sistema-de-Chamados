f74777b2c62a109443300a243487fdd9b76ca8d3
Ana Carolina <ana.carolina_95@hotmail.com>
2016-10-25 20:51:42 -0200

    Criação dos cenários de teste do pacote controller

--- Sistema-de-Chamados/test/com/br/es2/controller/ControleEmpresasTest.java
+++ Sistema-de-Chamados/test/com/br/es2/controller/ControleEmpresasTest.java
@@ -10,63 +10,63 @@ import static org.junit.Assert.*;
  * @author Ana Carolina
  */
 public class ControleEmpresasTest {
-    
+
     public ControleEmpresas ce;
-    
+
     public ControleEmpresasTest() {
     }
-    
+
     @Before
     public void setUp() {
         ce = new ControleEmpresas();
     }
-    
+
     @Test
-    public void testRetornaEmpresaExistente() {
+    public void testBuscaEmpresaExistente() {
         //cenário onde a Empresa buscada existe na base de dados (no arquivo empresas.dat)
         Empresa esperado = ce.retorna(1, "Mackenzie");
-        
+
         assertEquals("Mackenzie", esperado.getNomeEmpresa());
         assertEquals(1, esperado.getNumeroContrato());
     }
-    
+
     @Test
-    public void testRetornaEmpresaInexistente() {
+    public void testBuscaEmpresaQueNaoExiste() {
         //cenário onde a Empresa não existe na base de dados
         Empresa esperado = ce.retorna(2, "Universidade");
-        
+
         assertEquals(null, esperado);
     }
-    
+
     @Test
-    public void testValidarRetorno1() {
+    public void testVerificaNomeENumeroContratoEmUsoERetorna1() {
         //cenário onde não é possivel cadastrar a Empresa, pois número do contrato e o nome da empresa ja estao em uso
         //método retorna 1
     }
-    
+
     @Test
-    public void testValidarRetorno2() {
+    public void testVerificaNomeEmUsoERetorna2() {
         //cenário onde não é possivel cadastrar a Empresa, pois nome da empresa ja esta em uso
         //método retorna 2
     }
-    
+
     @Test
-    public void testValidarRetorno3() {
+    public void testVerificaNumeroContratoEmUsoERetorna3() {
         //cenário onde não é possivel cadastrar a Empresa, pois numero de contrato ja esta em uso
         //método retorna 3
     }
-    
+
     @Test
-    public void testValidarRetorno4() {
+    public void testVerificaNomeENumeroContratoDisponivelERetorna4() {
         //cenário onde é possível cadastrar a Empresa e metódo retorna 4
     }
 
     @Test
-    public void testInserirEmpresaCadastrada() {
+    public void testInserirEmpresaJaCadastrada() {
         //cenário que a Empresa já foi cadastrada na base de dados 
         //método deve retornar null
     }
-    
+
     @Test
     public void testInserirEmpresaCorretamente() {
         //cenário onde a Empresa ainda não foi cadastrada na base de dados 
@@ -79,7 +79,7 @@ public class ControleEmpresasTest {
         //cenário onde a Empresa existe na base de dados
         //método deve retornar true
     }
-    
+
     @Test
     public void testChecarEmpresaInexistente() {
         //cenário onde a Empresa não existe na base de dados
